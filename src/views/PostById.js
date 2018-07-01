8da1d08f3b61e8925011c007d5251719837f8203
romankhairov <roman.khairov@gmail.com>
2018-07-01 20:40:05 +0200

    finish PostById

--- src/views/PostById.js
+++ src/views/PostById.js
@@ -2,8 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { commentsFetch } from "../actions/comments/";
 import { postsFetch } from "../actions/posts";
+import isLoggedIn from "../components/isLoggedIn";
 
-import { Link } from "react-router-dom";
 
 const mapStateToProps = (state, props) => ({
   post: state.posts.posts.find(post => post.id === props.match.params.id),
@@ -14,32 +14,21 @@ class PostById extends React.Component {
 
    componentDidMount() {
     const id = this.props.match.params.id;
-    this.props.dispatch(commentsFetch(id));
     this.props.dispatch(postsFetch());
+    this.props.dispatch(commentsFetch(id));
   }
 
   render() {
 
-    if (!this.props.post) {
-      return "Loading..."
-    }
-
-    const comments = this.props.comments.map(comment => (
-      <div>
-        <h2>{comment.body}</h2>
-      </div>
-    ));
-
-    console.log(PostById);
+    const comments = this.props.comments.map(c => <p>{c.body} | { c.owner }<p>)
     return (
-        <div>
-          <h2>{ this.props.post.title }</h2>
-          <h1> Comments</h1>
-          { comments }
-          <img src={this.props.post.image} />
-        </div>
+      <div>
+        <img src={this.props.post.image} alt=""/>
+        { comments }
+      </div>
     );
+
   }
 }
 
-export default connect(mapStateToProps)(PostById);
+export default isLoggedIn(connect(mapStateToProps)(PostById));
